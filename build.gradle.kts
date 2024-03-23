45a9b04497a479c33e14d2be410df399ba23c2e4
Graham Daley <graham@daleybread.com>
2024-03-23 14:51:48 +0800

    Next is 3.0.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
 group = "io.github.grahamdaley"
-version = "3.0.0"
+version = "3.0.1-SNAPSHOT"
 
 object Meta {
     const val NAME = "lambda-runtime-graalvm"
